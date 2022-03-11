3b6c5ebc0730b4ce8458019420aea30e4963041d
vuongBriswell <vuong.nm@briswell-vn.com>
2022-03-11 12:44:31 +0700

    config

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.end(JSON.stringify({
-    data: 'Queen Võ thị Hồng Trang say Hello The World!'
+    data: 'Queen Võ thị Hồng Trang say Hello The World! Nguyễn Minh Vương'
   }));
 });
 
